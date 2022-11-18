9be6dd60954519b451e0dec317ab77b579d0a051
BeSae <ssa19@hi.is>
2022-11-18 01:43:56 +0000

    the canvas is not working

--- Verkefni10/main.js
+++ Verkefni10/main.js
@@ -1,11 +1,6 @@
-
-
-
 let canvas = document.querySelector("#canvas");
 let context = canvas.getContext("2d");
 let video = document.querySelector("#video");
-
-
 
 if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia){
   navigator.mediaDevices.getUserMedia({video: true}).then((stream) => {
